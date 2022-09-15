6fd7a513ecfeef50b27c8708fa9c5a5ae9de5430
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2022-09-15 12:19:44 +0600

    modal fixed

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = ((finishTime - startTime) / 1000);
+  const timeTaken = ((finishTime - startTime) / 1000).toFixed(0);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -79,9 +79,9 @@ const gameOver = () => {
   display.classList.add("inactive");
   // show result
   resultModal.innerHTML += `
-    <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+    <h1>Finished!</h1><br>
+    <p>You took: <span class="bold">${timeTaken}</span> seconds</p><br>
+    <p>You made <span class="bold red">${errorCount}</span> mistakes</p><br>
     <button onclick="closeModal()">Close</button>
   `;
 
@@ -134,8 +134,12 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = ((currentTime - startTime) / 1000).toFixed(0);
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
+
+window.onkeydown = function (e) {
+  return !(e.keyCode == 32);
+};
